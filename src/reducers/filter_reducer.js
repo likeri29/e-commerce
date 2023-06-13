5bce48292181de0341bcd864ef92c8d0e7326614
likeri29 <s.pochtaruk29@gmail.com>
2023-06-13 18:26:44 +0200

    add sort functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,6 +17,38 @@ const filter_reducer = (state, action) => {
         allProducts: [...action.payload],
         filteredProducts: [...action.payload],
       };
+
+    case SET_GRIDVIEW:
+      return { ...state, gridView: true };
+
+    case SET_LISTVIEW:
+      return { ...state, gridView: false };
+
+    case UPDATE_SORT:
+      return { ...state, sort: action.payload };
+
+    case SORT_PRODUCTS:
+      const { sort, filteredProducts } = state;
+      let tempProducts = [...filteredProducts];
+
+      if (sort === "price-lowest") {
+        tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+      }
+      if (sort === "price-highest") {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+      }
+      if (sort === "name-a") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (sort === "name-z") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return { ...state, filteredProducts: tempProducts };
+
     default:
       throw new Error(`No Matching "${action.type}" - action type`);
   }
