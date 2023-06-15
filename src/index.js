556da7b1359e56eec5d975102dfde1d034058d68
likeri29 <s.pochtaruk29@gmail.com>
2023-06-15 16:48:38 +0200

    add login/logout functionality

--- src/index.js
+++ src/index.js
@@ -7,16 +7,26 @@ import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, User } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-43j35csx17mj787o.us.auth0.com"
+    clientId="KXEYcc8vSGwes5HJa6Z7yzRhvJcgs1U2"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
