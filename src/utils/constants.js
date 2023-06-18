aba000565940272f460a6375a65c8cc7bd12e5c2
likeri29 <s.pochtaruk29@gmail.com>
2023-06-18 15:59:09 +0200

    change from API to Airtable, change products and font

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -39,6 +39,10 @@ export const services = [
   },
 ];
 
-export const products_url = "https://course-api.com/react-store-products";
+//changed API response form course API to Airtable
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+// export const products_url = "https://course-api.com/react-store-products";
+export const products_url = "/.netlify/functions/products";
+
+// export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+export const single_product_url = `/.netlify/functions/single-product?id=`;
